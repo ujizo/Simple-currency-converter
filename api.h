e717d8b20e661c4d31dff10befea73c23a9463e6
pyerk <arbuzees@icloud.com>
2026-02-12 18:53:45 +0300

    Update api.h

--- api.h
+++ api.h
@@ -4,13 +4,9 @@
 #include <string>
 
 class Api {
-private:
-    std::string base_url = "https://api.exchangerate-api.com/v4/latest/";
-
-    std::string httpGet(const std::string& url);
-
 public:
     double getRate(const std::string& from, const std::string& to);
+    std::string fetchExchangeRates(); 
 };
 
 #endif
