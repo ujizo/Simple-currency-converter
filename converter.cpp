dba501b03d5a77a3ec575e802ae96f87620da459
pyerk <arbuzees@icloud.com>
2026-02-12 19:19:44 +0300

    Update converter.cpp

--- converter.cpp
+++ converter.cpp
@@ -12,25 +12,23 @@ Converter::~Converter() {
 
 double Converter::fetchRate(const std::string& from, const std::string& to) {
     std::string key = from + "_" + to;
-
-    // Проверяем кэш
+    
     auto it = cache.find(key);
     if (it != cache.end()) {
         return it->second;
     }
-
-
+    
     double rate = api.getRate(from, to);
     if (rate > 0) {
         cache[key] = rate;
     }
-
+    
     return rate;
 }
 
 double Converter::convert(double amount, const std::string& from, const std::string& to) {
     if (from == to) return amount;
-
+    
     double rate = fetchRate(from, to);
     return amount * rate;
 }
