c3fb54c5d157043069f054c401af49cb529daf53
pyerk <arbuzees@icloud.com>
2026-02-12 18:07:47 +0300

    Update converter.cpp
    обновли converter

--- converter.cpp
+++ converter.cpp
@@ -1,38 +1,37 @@
 #include "converter.h"
+#include <curl/curl.h>
 #include <iostream>
-#include <chrono>
 
 Converter::Converter() {
     curl_global_init(CURL_GLOBAL_DEFAULT);
 }
 
+Converter::~Converter() {
+    curl_global_cleanup();
+}
+
 double Converter::fetchRate(const std::string& from, const std::string& to) {
+    std::string key = from + "_" + to;
+
+    // Проверяем кэш
+    auto it = cache.find(key);
+    if (it != cache.end()) {
+        return it->second;
+    }
+
+
     double rate = api.getRate(from, to);
-    
     if (rate > 0) {
-        std::string key = from + "_" + to;
         cache[key] = rate;
     }
-    
+
     return rate;
 }
 
 double Converter::convert(double amount, const std::string& from, const std::string& to) {
     if (from == to) return amount;
-    
-    std::string key = from + "_" + to;
-    
-    if (cache.find(key) != cache.end()) {
-        return amount * cache[key];
-    }
-    
+
     double rate = fetchRate(from, to);
-    
-    if (rate < 0) {
-        std::cerr << "Error: Could not get exchange rate" << std::endl;
-        return -1;
-    }
-    
     return amount * rate;
 }
 
