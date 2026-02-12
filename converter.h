95cac2aa82441ddb37d6504453806950aeaf06a1
pyerk <arbuzees@icloud.com>
2026-02-12 18:08:21 +0300

    Update converter.h
    обновил converter.h

--- converter.h
+++ converter.h
@@ -3,18 +3,18 @@
 
 #include <string>
 #include <map>
+#include "api.h"
 
 class Converter {
 private:
     Api api;
     std::map<std::string, double> cache;
-    
-    double fetchRate(const std::string& from, const std::string& to);
-    
+
 public:
     Converter();
-    
+    ~Converter();
     double convert(double amount, const std::string& from, const std::string& to);
+    double fetchRate(const std::string& from, const std::string& to);
     void clearCache();
 };
 
